c41ca633205e66d1640975052653183fd48fb685
abdulsatertn <abdulsatertn246@gmail.com>
2024-03-27 13:01:58 -0700

    Add SplashView and SlidingText widgets
    Added SplashView widget to display a splash screen and SlidingText widget
    for animated sliding text effect in the splash screen.

---
+++ lib/Features/splash/presentation/views/widgets/sliding_text.dart
@@ -0,0 +1,28 @@
+import 'package:bookly_app/core/utils/strings.dart';
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    super.key,
+    required this.slidingAnimation,
+  });
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    // using the animatedbulder so i just rebuildt the text and there is no need to use set state and rebuild the whole screen
+
+    return AnimatedBuilder(
+        animation: slidingAnimation,
+        builder: (context, _) {
+          return SlideTransition(
+            position: slidingAnimation,
+            child: const Text(
+              AppStrings.splashText,
+              textAlign: TextAlign.center,
+            ),
+          );
+        });
+  }
+}
