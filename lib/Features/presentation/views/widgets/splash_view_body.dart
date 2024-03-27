8f0b0ac13f256f3e7f932f3151cfe20e150ef8c2
abduustn <115055703+abduustn@users.noreply.github.com>
2024-03-27 06:09:32 -0700

    Add sliding text animation to SplashViewBody widget
    Import SlidingText widget and add sliding text animation functionality
    to the SplashViewBody widget in order to enhance the user experience.

--- lib/Features/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,43 @@
+import 'package:bookly_app/Features/presentation/views/widgets/sliding_text.dart';
 import 'package:bookly_app/core/utils/app_sizes.dart';
 import 'package:bookly_app/core/utils/assets.dart';
-import 'package:bookly_app/core/utils/strings.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+    // slidingAnimation.addListener(() {
+    //   setState(() {});
+    // });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +47,7 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Image.asset(AssetsData.logo),
         const SizedBox(height: AppSizes.s4),
-        const Text(
-          AppStrings.splashText,
-          textAlign: TextAlign.center,
-        ),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
