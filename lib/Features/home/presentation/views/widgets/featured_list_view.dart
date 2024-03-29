db1eb8639e59efa5f7e7c4dd982ce36855becfae
abdulsatertn <abdulsatertn246@gmail.com>
2024-03-29 05:07:50 -0700

    Add custom book image widget and update usage in book details view
    - Added a new custom book image widget for displaying book images
    - Updated the book details view to use the custom book image widget for better
      image display and consistency.

--- lib/Features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_app/Features/home/presentation/views/widgets/featured_list_view_item.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/constants_classes.dart';
@@ -15,7 +15,7 @@ class FeaturedBooksListView extends StatelessWidget {
           itemBuilder: (context, index) {
             return const Padding(
               padding: EdgeInsets.symmetric(horizontal: AppPadding.p8),
-              child: FeaturedListViewItem(),
+              child: CustomBookImage(),
             );
           }),
     );
