800be064095daf8e7ad5dc528153afe6649200b2
abdulsatertn <abdulsatertn246@gmail.com>
2024-03-31 07:07:54 -0700

    Add newest books cubit and state classes
    Added newest_books_cubit.dart and newest_books_state.dart files with
    NewestBooksCubit and NewestBooksState classes to manage the state of
    newest books feature in the home presentation manager.

---
+++ lib/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit() : super(NewestBooksInitial());
+}
