c25228ed36e0dea4de497f80671bdbf482647591
abdulsatertn <abdulsatertn246@gmail.com>
2024-03-31 04:20:14 -0700

    implement fetch newest books
    Update fetchBestSellerBooks method to fetchNewestBooks
    
    - Updated the fetchBestSellerBooks method in HomeRepo to fetchNewestBooks
      to align with the actual functionality implemented in HomeRepoImpl.
    - Added implementation for fetchNewestBooks in HomeRepoImpl to fetch the
      newest books related to programming from the Google Books API.
    - Updated ApiService class to use the private _dio instance variable for
      making API requests.

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -1,12 +1,33 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {}
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoints:
+              'volumes?q=subject:Programming&Filtering=free-ebooks&Sorting=newest');
+
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
+  }
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
