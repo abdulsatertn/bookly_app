6583223b7b47e1a39cda833e201c976e1756cbd3
abdulsatertn <abdulsatertn246@gmail.com>
2024-04-03 08:42:42 -0700

    Add SimilarBooksCubit for fetching similar books and update views
    - Added SimilarBooksCubit to fetch similar books based on category.
    - Updated BookDetailsView to initialize SimilarBooksCubit and fetch data.
    - Updated SimilarBooksListView to display similar books with error handling.
    - Updated AppRouter to provide SimilarBooksCubit to BookDetailsView.

---
+++ lib/Features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart
@@ -0,0 +1,33 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
+import 'package:bookly_app/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+import '../../../data/models/book_model/book_model.dart';
+
+part 'similar_books_state.dart';
+
+class SimilarBooksCubit extends Cubit<SimilarBooksState> {
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchSimilardBooks({required String category}) async {
+    emit(SimilarBooksLoading());
+    var result = await homeRepo.fetchSimilarBooks(category: category);
+
+    result.fold(
+      (failure) => emit(SimilarBooksFailure(failure.errorMessage)),
+      (books) => emit(SimilarBooksSuccess(books)),
+    );
+  }
+
+  @override
+  void onChange(Change<SimilarBooksState> change) {
+    super.onChange(change);
+    if (kDebugMode) {
+      print(change);
+    }
+  }
+}
--- lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,24 +1,38 @@
+import 'package:bookly_app/Features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:bookly_app/core/utils/constants_classes.dart';
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indecator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimilarBooksListView extends StatelessWidget {
   const SimilarBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .15,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: AppPadding.p5),
-              child: CustomBookImage(
-                imageUrl: 'https://via.placeholder.com/200',
-              ),
-            );
-          }),
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+      builder: (context, state) {
+        if (state is SimilarBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .15,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: AppPadding.p5),
+                    child: CustomBookImage(
+                      imageUrl: 'https://via.placeholder.com/200',
+                    ),
+                  );
+                }),
+          );
+        } else if (state is SimilarBooksFailure) {
+          return CustomErrorWidget(errorMessage: state.errorMessage);
+        } else {
+          return const CustomLoadingIndecator();
+        }
+      },
     );
   }
 }
